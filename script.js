8665073ca38d003333aa2cd26a243d0f0f572c68
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-07-02 18:45:14 +0000

    Alterei o script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaPrincipal = document.querySelector(".caixa-principal") ;
+const caixaPerguntas = document.querySelector(".caixa-perguntas") ;
+const caixaAlternativas = document.querySelector(".caixa-alternativas") ;
+const caixaResultado = document.querySelector(".caixa-resultado") ;
+const textoResultado = document.querySelector(".texto-resultado") ;
+
+const perguntas =[
+    {
+        enunciado : "No Brasil, em 2022, a média nacional apontou que 31,2% dos adolescentes estavam com excesso de peso. Qual o primeiro pensamento?",
+        alternativas : [
+            "Isso é preocupante!",
+            "Isso é bom!"
+        ]
+    },
+    {
+        enunciado : "As estratégias de gamificação podem ser implementadas para conscientizar adolescentes sobre a adoção de práticas sustentáveis, como o uso de energia renovável, enquanto também incentivam hábitos saudáveis para combater a obesidade na adolescência?",
+        alternativas : [
+            "É  importante a utilização de estratégias de gamificação, tais como: plataformas no ambiente escolar.",
+            "A utilização de estratégias de gamificação não seriam eficazes para conscientizar os alunos."
+        ]
+    },
+    {
+        enunciado : "QUAL A IMPORTÂNCIA DAS ENERGIAS RENOVÁVEIS PARA O MUNDO?",
+        alternativas : [
+            "É MUITO IMPORTANTE PARA O MUNDO ATUAL.",
+            "NÃO TEM TANTA IMPORT NCIA ASSIM."
+        ]
+    }
+] ;
