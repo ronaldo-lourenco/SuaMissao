11ebbbdb301ff65d27f78a614720684973857e41
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-09-03 19:48:31 +0000

    Atualização JS

--- script.js
+++ script.js
@@ -1,40 +1,87 @@
-const caixaPrincipal = document.querySelector(".caixa-principal") ;
-const caixaPerguntas = document.querySelector(".caixa-perguntas") ;
+const caixaPrincipal    = document.querySelector(".caixa-principal") ;
+const caixaPerguntas    = document.querySelector(".caixa-perguntas") ;
 const caixaAlternativas = document.querySelector(".caixa-alternativas") ;
-const caixaResultado = document.querySelector(".caixa-resultado") ;
-const textoResultado = document.querySelector(".texto-resultado") ;
+const caixaResultado    = document.querySelector(".caixa-resultado") ;
+const textoResultado    = document.querySelector(".texto-resultado") ;
 const perguntas =[
     {
         enunciado : "No Brasil, em 2022, a média nacional apontou que 31,2% dos adolescentes estavam com excesso de peso. Qual o primeiro pensamento?",
         alternativas : [
-            "Isso é preocupante!",
-            "Isso é bom!"
+            {
+            texto: "Isso é preocupante!",
+            afirmacao: "Ficou com receio de estar passando por essa situação." 
+            },
+            {
+            texto: "Isso é bom!",
+            afirmacao: "Pensou que pelo  menos agora os adolescentes não tem mais problemas para se alimentar."
+            }    
         ]
     },
     {
         enunciado : "As estratégias de gamificação podem ser implementadas para conscientizar adolescentes sobre a adoção de práticas sustentáveis, como o uso de energia renovável, enquanto também incentivam hábitos saudáveis para combater a obesidade na adolescência?",
         alternativas : [
-            "É  importante a utilização de estratégias de gamificação, tais como: plataformas no ambiente escolar.",
-            "A utilização de estratégias de gamificação não seriam eficazes para conscientizar os alunos."
+            {
+            texto: "É  importante a utilização de estratégias de gamificação, tais como: plataformas no ambiente escolar.",
+            afirmacao: "É um processo mais atraente  e estimula mudanças comportamentais ao integrar a aprendizagem com a aplicação prática do cotidiano do aluno e o uso de tecnologias."
+            },
+            {
+            texto: "A utilização de estratégias de gamificação não seriam eficazes para conscientizar os alunos.",
+            afirmacao: "Preocupado com que a abordagem possa simplificar questões complexas, reduzindo  a reflexão crítica dos alunos, devido a tendência pela educação tradicional."
+            }
         ]
     },
     {
         enunciado : "QUAL A IMPORTÂNCIA DAS ENERGIAS RENOVÁVEIS PARA O MUNDO?",
         alternativas : [
-            "É MUITO IMPORTANTE PARA O MUNDO ATUAL.",
-            "NÃO TEM TANTA IMPORT NCIA ASSIM."
+            {
+            texto: "É MUITO IMPORTANTE PARA O MUNDO ATUAL.",
+            afirmacao: "SÃO OPÇÕES INESGOTÁVEIS E SUSTENTÁVEIS SEM GERAR RESÍDUOS"
+            },
+            {
+            texto: "NÃO TEM TANTA IMPORT NCIA ASSIM.",
+            afirmacao: "FALTA CONHECIMENTO PARA AS QUESTÕES AMBIENTAIS ATUAIS"
+            }
         ]
     }
 ] ;
 
 let atual = 0 ;
 let perguntaAtual ;
+let historiaFinal = "" ;
 
 function mostraPergunta(){
-    perguntaAtual = perguntas[atual].enunciado ;
-    caixaPerguntas.textContent = perguntaAtual ;
-};
+    if (atual >= perguntas.length){
+        mostraResultado() ;
+        return ;
+    } ;
+    perguntaAtual = perguntas[atual] ;
+    caixaPerguntas.textContent    = perguntaAtual.enunciado ;
+    caixaAlternativas.textContent = "" ;
+    textoResultado.textContent    = "" ;
+    mostraAlternativas() ;
+} ;
+
+function mostraAlternativas(){
+    for (const alternativa of perguntas[atual].alternativas){
+        const botaoAlternativas = document.createElement("button") ;
+        botaoAlternativas.textContent = alternativa.texto ;
+        botaoAlternativas.addEventListener("click", ()=>respostaSelecionada(alternativa)) ;
+        caixaAlternativas.appendChild(botaoAlternativas) ;
+    } 
+} ;
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao ;
+    historiaFinal += afirmacao + " " ;
+    atual++ ;
+    mostraPergunta() ;
+} ;
 
 mostraPergunta() ;
 
+function mostraResultado(){
+    caixaPerguntas.textContent    = "Em 2049..." ;
+    textoResultado.textContent    = historiaFinal ;
+    caixaAlternativas.textContent = "" ;
+}
 
